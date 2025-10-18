fa2e41d541359b9ae18865b512e8cf5924cab6f3
wanda-dev6 <avilabenta6@gmail.com>
2025-10-17 23:45:09 -0500

    Exercise_7

---
+++ ExerciseSeven.py
@@ -0,0 +1,14 @@
+#weight= int(input("¿Usted cuanto pesa? :"))
+#stature= float(input("¿Cual es su estatura?"))
+#imc= weight/(stature**2)
+#print(f"Su IMC es de: {imc}")
+#name= ("Pinochio")
+#nose= ("nariz")
+#tall= ("y mide 12 centimetros")
+#print(f"{name} tiene la {nose} grande {tall}")
+
+number:int="12"
+ope=number+1
+print(ope)
+
+
