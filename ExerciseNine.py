48d3be2c121a1983f5200c28c3a22a40b8c9ad6b
wanda-dev6 <avilabenta6@gmail.com>
2025-10-20 17:37:17 -0500

    Exercise_9

---
+++ ExerciseNine.py
@@ -0,0 +1,11 @@
+investment= int(input("¿Cuanto invertira?:"))
+annual= float(input("¿Cual es el interes anual?:"))/100
+Years= int(input("¿Cual es su numero de años a invertir?:"))
+capital= investment* (1 + annual) ** Years
+ganancia = capital-investment
+print(f"la ganancia obtenida es de: {ganancia}")
+
+
+
+
+
