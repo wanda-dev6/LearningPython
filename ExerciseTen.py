4f2aff878a74673928a817c9b6bc0362e1143620
wanda-dev6 <avilabenta6@gmail.com>
2025-10-22 21:43:50 -0500

    Exercise_10

---
+++ ExerciseTen.py
@@ -0,0 +1,7 @@
+clowns=112
+dolls=75
+number_1=int(input("El numero de muñecas vendido en el ultimo pedido es de:"))
+number_2=int(input("El numero de payasos vendido en el ultimo pedido es de:"))
+multiplicacion_1=number_1*dolls
+multiplicacion_2=number_2*clowns
+print(f"El peso total del paquete de muñecas es de: {multiplicacion_1} y el peso total del paquete de payasos es de: {multiplicacion_2}")
