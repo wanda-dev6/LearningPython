453af3844e18d30459da9f3bff97128c80f32316
wanda-dev6 <avilabenta6@gmail.com>
2025-10-23 18:01:01 -0500

    Exercise_11

---
+++ ExerciseEleven.py
@@ -0,0 +1,18 @@
+
+savings= int(input("¿Cual es la cantidad depositada?:"))
+year_1=savings*4/100
+year_2=savings*8/100
+year_3=savings*12/100
+redondeo_1=round(year_1,2)
+redondeo_2=round(year_2,2)
+redondeo_3=round(year_3,2)
+
+print(f"El ahorro del primer año seria de: {redondeo_1}")
+print(f"El ahorro del segundo año seria de: {redondeo_2}")
+print(f"El ahorro del tercer año seria de: {redondeo_3}")
+
+
+
+
+
+
