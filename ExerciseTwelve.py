3d8549af929f194afe43b7edb0b21f3cc1fe74c8
wanda-dev6 <avilabenta6@gmail.com>
2025-10-24 18:36:50 -0500

    Exercise_12

---
+++ ExerciseTwelve.py
@@ -0,0 +1,5 @@
+pan=float(input("¿Cual es el numero de barras que no son del dia?"))
+preciosbarras= 3.49*pan
+panmalo=60
+descuento= preciosbarras*panmalo/100
+print(f"El descuento que se le hace a la barra de pan es de: {descuento}")
