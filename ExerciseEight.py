b42c0067ee751e7a7191edb271ab1ee6be447c10
wanda-dev6 <avilabenta6@gmail.com>
2025-11-02 23:46:49 -0500

    Actualizar ExerciseEight.py

--- ExerciseEight.py
+++ ExerciseEight.py
@@ -3,7 +3,7 @@ Number_2= int(input("segundo numero:"))
 Cociente= Number_1//Number_2
 Residuo= Number_1%Number_2
 name= "wanda"
-print(f"hola {name} el cociente es {Cociente} y el residuo es {Residuo}")
+print(f"hola {name} el cociente es {Cociente} y el residuo es {Residuo}"). 
 
 
 
