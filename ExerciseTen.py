5efa3b4a27627808f491cafe6028e5825d595e99
wanda-dev6 <avilabenta6@gmail.com>
2025-11-27 23:49:09 -0500

    Actualizar ExerciseTen.py

--- ExerciseTen.py
+++ ExerciseTen.py
@@ -5,3 +5,5 @@ number_2=int(input("El numero de payasos vendido en el ultimo pedido es de:"))
 multiplicacion_1=number_1*dolls
 multiplicacion_2=number_2*clowns
 print(f"El peso total del paquete de muñecas es de: {multiplicacion_1} y el peso total del paquete de payasos es de: {multiplicacion_2}")
+
+:3
